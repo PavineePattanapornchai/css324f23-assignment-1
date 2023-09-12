e33b02f856dac47c7fb548b4e8d7280927217388
PavineePattanapornchai <144767086+PavineePattanapornchai@users.noreply.github.com>
2023-09-12 20:54:15 +0700

    Homework1
    6422782266 Pavinee Pattanapornchai

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,31 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)  # Change initial state to (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s == (4, 4, 0)  # Change the goal check
 
 def successors(s):
     x, y, z = s
-    return []
+    actions = []
+    
+    # Define valid pouring actions and their corresponding costs
+    if x > 0 and y < 5:
+        amount_to_pour = min(x, 5 - y)
+        actions.append(((x - amount_to_pour, y + amount_to_pour, z), amount_to_pour))
+    if x > 0 and z < 3:
+        amount_to_pour = min(x, 3 - z)
+        actions.append(((x - amount_to_pour, y, z + amount_to_pour), amount_to_pour))
+    if y > 0 and x < 8:
+        amount_to_pour = min(y, 8 - x)
+        actions.append(((x + amount_to_pour, y - amount_to_pour, z), amount_to_pour))
+    if y > 0 and z < 3:
+        amount_to_pour = min(y, 3 - z)
+        actions.append(((x, y - amount_to_pour, z + amount_to_pour), amount_to_pour))
+    if z > 0 and x < 8:
+        amount_to_pour = min(z, 8 - x)
+        actions.append(((x + amount_to_pour, y, z - amount_to_pour), amount_to_pour))
+    if z > 0 and y < 5:
+        amount_to_pour = min(z, 5 - y)
+        actions.append(((x, y + amount_to_pour, z - amount_to_pour), amount_to_pour))
+    
+    return actions
